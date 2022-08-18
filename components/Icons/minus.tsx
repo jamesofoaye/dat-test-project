5ee528129f75a40ebda16c9e650b4a4b58e000bb
James Ofori Ayerakwa <58654469+jamesofoaye@users.noreply.github.com>
2022-08-18 16:30:22 +0400

    chore: created Minus Icon Component

---
+++ components/Icons/minus.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export const Minus = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+     
+    >
+      <rect width="12" height="2" x="6" y="11" fill="#292929" rx="1"></rect>
+    </svg>
+  );
+};
+
