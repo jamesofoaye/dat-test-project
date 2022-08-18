8028373479bb0d9681bbce6fe3a0fb6b991c05f6
James Ofori Ayerakwa <58654469+jamesofoaye@users.noreply.github.com>
2022-08-18 22:32:08 +0400

    chore: added api hostname to images domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: ["54.169.31.224"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
