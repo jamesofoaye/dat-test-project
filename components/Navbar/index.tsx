5709727a3b8117eb7939cf4429f2ebd86416260f
James Ofori Ayerakwa <58654469+jamesofoaye@users.noreply.github.com>
2022-08-19 14:55:06 +0400

    prettier formatted

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,44 +1,42 @@
-import Link from 'next/link';
-import { Logo, Cart } from '../Icons';
-import styles from './navbar.module.scss'
+import Link from "next/link";
+import { Logo, Cart } from "../Icons";
+import styles from "./navbar.module.scss";
 
-const Navbar = () =>  {
-  return (
-    <nav className={styles.container}>
-        <Logo />
+const Navbar = () => {
+	return (
+		<nav className={styles.container}>
+			<Logo />
 
-        <ul className={styles.links}>
-            <li>
-                <Link href="/">
-                    <a>Home</a>
-                </Link>
-            </li>
+			<ul className={styles.links}>
+				<li>
+					<Link href="/">
+						<a>Home</a>
+					</Link>
+				</li>
 
-            <li>
-                <Link href="/#form">
-                    <a>Form</a>
-                </Link>
-            </li>
+				<li>
+					<Link href="/#form">
+						<a>Form</a>
+					</Link>
+				</li>
 
-            <li>
-                <Link href="/#menu">
-                    <a>Menu</a>
-                </Link>
-            </li>
-        </ul>
+				<li>
+					<Link href="/#menu">
+						<a>Menu</a>
+					</Link>
+				</li>
+			</ul>
 
-        <div className={styles.cartSection}>
-            <div className={styles.cart}>
-                <Cart />
-                <p className={styles.cartTotal}>2</p>
-            </div>
+			<div className={styles.cartSection}>
+				<div className={styles.cart}>
+					<Cart />
+					<p className={styles.cartTotal}>2</p>
+				</div>
 
-            <button className={styles.button}>
-                Log In
-            </button>
-        </div>
-    </nav>
-  );
-}
+				<button className={styles.button}>Log In</button>
+			</div>
+		</nav>
+	);
+};
 
 export default Navbar;
