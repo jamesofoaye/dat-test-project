461b188fea73d36dec664d0ae49bf9357770a4e2
James Ofori Ayerakwa <58654469+jamesofoaye@users.noreply.github.com>
2022-08-19 15:52:52 +0400

    feat: added cart modal skeleton and cart functionality

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { Logo, Cart } from "../Icons";
 import styles from "./navbar.module.scss";
+import { Minus, Plus } from "../Icons";
+import { useCart } from "react-use-cart";
 
 const Navbar = () => {
+	const {
+		isEmpty,
+		cartTotal,
+		totalUniqueItems,
+		items,
+		updateItemQuantity,
+		removeItem,
+	} = useCart();
+
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<nav className={styles.container}>
 			<Logo />
@@ -28,13 +43,80 @@ const Navbar = () => {
 			</ul>
 
 			<div className={styles.cartSection}>
-				<div className={styles.cart}>
+				<div className={styles.cart} onClick={() => setIsOpen(!isOpen)}>
 					<Cart />
-					<p className={styles.cartTotal}>2</p>
+					<p className={styles.cartTotal}>{isEmpty ? 0 : totalUniqueItems}</p>
 				</div>
 
 				<button className={styles.button}>Log In</button>
 			</div>
+
+			{isOpen && (
+				<div className={styles.cartMenu}>
+					<h1 className={styles.cartMenuTitle}>Cart</h1>
+
+					{items.map((item: any, index: number) => (
+						<div key={index}>
+							<div className={styles.cartMenuItem}>
+								<div className={styles.cartMenuItemImg}>
+									<Image
+										src={item.image}
+										alt={item.title}
+										width={110}
+										height={85}
+									/>
+								</div>
+
+								<div className={styles.cartMenuItemInfo}>
+									<p className={styles.cartMenuItemName}>{item.title}</p>
+
+									<div className={styles.cartMenuItemActions}>
+										<button
+											className={styles.cartMenuItemButton}
+											onClick={() =>
+												updateItemQuantity(item.id, item.quantity - 1)
+											}
+										>
+											<Minus />
+										</button>
+
+										<p className={styles.cartMenuItemQuantity}>
+											{item.quantity}
+										</p>
+
+										<button
+											className={styles.cartMenuItemButton}
+											onClick={() =>
+												updateItemQuantity(item.id, item.quantity + 1)
+											}
+										>
+											<Plus />
+										</button>
+
+										<p className={styles.cartMenuItemPrice}>
+											{item.currency} {item.price}
+										</p>
+									</div>
+								</div>
+							</div>
+
+							<button
+								className={styles.cartMenuItemRemove}
+								onClick={() => removeItem(item.id)}
+							>
+								Remove
+							</button>
+						</div>
+					))}
+
+					<button className={styles.cartMenuItemCheckout}>
+						Check Out
+						<span className={styles.cartMenuItemCheckoutTotal}>
+							{cartTotal}
+						</span>
+					</button>
+				</div>
+			)}
 		</nav>
 	);
 };
